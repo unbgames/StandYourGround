633275742fd859c3634976b364a99c3b8fd126fd
lucasm0ta <lmotaribeiro@gmail.com>
2018-06-07 09:25:25 -0300

    Fix multiple sound at samen time.

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -5,7 +5,7 @@
 #include "../include/Sound.h"
 
 Sound::Sound(GameObject &associated, bool shouldReplay): Component(associated), started(false),
-  chunk(nullptr), volume(MIX_MAX_VOLUME), channel(0), shouldReplay(shouldReplay) {
+  chunk(nullptr), volume(MIX_MAX_VOLUME), shouldReplay(shouldReplay) {
 }
 
 Sound::Sound(GameObject &associated, std::string file, bool shouldReplay) : Sound(associated, shouldReplay) {
@@ -17,17 +17,25 @@ Sound::~Sound() {
 
 void Sound::Play(int times) {
     if (chunk != nullptr) {
-        if (shouldReplay) {
+        if (!shouldReplay) {
+            std::cout << "Stopped Sound" << std::endl;
             Stop();
         }
-        if (!started || !Playing()) {
-            int channel = Mix_PlayChannel(-1, chunk.get(), 0);
-            if (channel > -1) {
-                Mix_Volume(channel, volume);
-                started = true;
+        int channel = Mix_PlayChannel(-1, chunk.get(), 0);
+        if (channel > -1) {
+            Mix_Volume(channel, volume);
+            started = true;
+            auto it = channels.begin();
+            while (it != channels.end()) {
+                if (Mix_Playing(*it) == 1) {
+                    it = channels.erase(it);
+                } else {
+                    it++;
+                }
             }
-            // std::cerr<< "Sound at channel:"<<channel<<" at "<<&(*this)<<std::endl;
+            channels.push_back(channel);
         }
+            // std::cerr<< "Sound at channel:"<<channel<<" at "<<&(*this)<<std::endl;
     } else {
         std::cerr<< "No sound to be played"<<std::endl;
     }
@@ -37,26 +45,26 @@ bool Sound::Playing() {
     // std::cout<<"Channel:"<<channel<<std::endl;
     // std::cout<<"Ta tocando"<<(Mix_Playing(channel)?"True":"False")<<std::endl;
     bool isPlaying = false;
-    for (auto iter = channels.begin(); iter != channels.end();) {
-        if (Mix_Playing(channels[*iter]) == 1) {
-            isPlaying = true;
-            ++iter;
+
+    auto it = channels.begin();
+    while (it != channels.end()) {
+        if (Mix_Playing(*it) == 1) {
+            it = channels.erase(it);
         } else {
-            channeld.erase(iter);
+            isPlaying = true;
+            it++;
         }
     }
     return (started && isPlaying);
 }
 
 void Sound::Stop() {
-    if (chunk != nullptr && Playing()) {
-        // std::cout<<"Channel "<<channel<<" HALTED"<<std::endl;
-        //
+    if (chunk != nullptr) {
         for (const int &channel : channels) {
             Mix_HaltChannel(channel);
-            started = false;
         }
         channels.clear();
+        started = false;
     }
 }
 
@@ -68,7 +76,9 @@ void Sound::Open(std::string file) {
 void Sound::SetVolume(unsigned int vol) {
     volume = vol;
     if (started && Playing()) {
-        Mix_Volume(channel, volume);
+        for (const int &channel : channels) {
+            Mix_Volume(channel, volume);
+        }
     }
 }
 
